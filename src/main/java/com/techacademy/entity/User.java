3b5c10571397c9bc3e1ccf36a1ff34d7d3d2d30b
ryutaro-shimota <ryutaro.2000.radiology@gmail.com>
2023-03-20 20:46:27 +0900

    update Project

---
+++ src/main/java/com/techacademy/entity/User.java
@@ -0,0 +1,74 @@
+package com.techacademy.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne; 
+import javax.persistence.PreRemove; 
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    /** 性別用の列挙型 */
+    public static enum Gender {
+        男性, 女性
+    }
+
+    /** 主キー。自動生成 */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /** 名前。20桁。null不許可 */
+    @Column(length = 20, nullable = false)
+    @NotEmpty
+    @Length(max=20)
+    private String name;
+
+    /** 性別。2桁。列挙型（文字列） */
+    @Column(length = 2)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Gender gender;
+
+    /** 年齢 */
+    @Min(0)
+    @Max(120)
+    private Integer age;
+
+    /** メールアドレス。50桁。null許可 */
+    @Column(length = 50)
+    @Email
+    @Length(max=50)
+    private String email;
+
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    /** レコードが削除される前に行なう処理 */
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if (authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
+}
---
+++ src/test/java/com/techacademy/controller/UserControllerTest2.java
@@ -0,0 +1,73 @@
+package com.techacademy.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.techacademy.entity.User;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+class UserControllerTest2 {
+    private MockMvc mockMvc;
+
+    private final WebApplicationContext webApplicationContext;
+
+    UserControllerTest2(WebApplicationContext context) {
+        this.webApplicationContext = context;
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        // Spring Securityを有効にする
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .apply(springSecurity()).build();
+    }
+
+    @Test
+    @DisplayName("userlist")
+    @WithMockUser
+    void testGetUserlist() throws Exception {
+        // HTTPリクエストに対するレスポンスの検証
+        MvcResult result = mockMvc.perform(get("/user/list")) // URLにアクセス
+            .andExpect(status().isOk()) // ステータスを確認
+            .andExpect(model().attributeExists("userlist")) // Modelの内容を確認
+            .andExpect(model().hasNoErrors()) // Modelのエラー有無の確認
+            .andExpect(view().name("user/list")) // viewの確認
+            .andReturn(); // 内容の取得
+
+        // userの検証
+        // Modelからuserlistを取り出す
+        List<User> userlist = (List<User>)result.getModelAndView().getModel().get("userlist");
+        assertEquals(userlist.size(), 3);
+        User user1= userlist.get(0);
+        assertEquals(user1.getId(), 1);
+        assertEquals(user1.getName(), "キラメキ太郎");
+        User user2 = userlist.get(1);
+        assertEquals(user2.getId(), 2);
+        assertEquals(user2.getName(), "キラメキ次郎");
+        User user3 = userlist.get(2);
+        assertEquals(user3.getId(), 3);
+        assertEquals(user3.getName(), "キラメキ花子");
+    }
+}
